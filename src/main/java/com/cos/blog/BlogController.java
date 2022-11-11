e95aad567ddbfd153a641869b0fdf2d768030826
devchankim <chankim.dev@gmail.com>
2022-11-11 04:18:31 -0500

    Test HTTP Request

--- src/main/java/com/cos/blog/BlogController.java
+++ src/main/java/com/cos/blog/BlogController.java
@@ -1,4 +1,4 @@
-package com.cos.blog.test;
+package com.cos.blog;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 // 스프링이 com.cos.blog 패키지 이하를 스캔해서 모든파일을 메모리에 new하는 것은 아니고
 // 특정 어노테이션이 붙어있는 클래스 파일들을 new해서(IoC) 스프링 컨테이너에서 관리해줍니다.
 @RestController 
-public class BlogControllerTest {
+public class BlogController {
 	
 	@GetMapping("/test/hello")
 	public String hello() {
